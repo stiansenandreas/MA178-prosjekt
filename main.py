a764c0e5db21cbcaf15684f5b1312416a0f598fc
stiansenandreas <stiansenandreas@gmail.com>
2021-10-21 18:27:56 +0200

    .

--- main.py
+++ main.py
@@ -13,25 +13,19 @@ def newton_raphson(x0, f, df):
         if df(x0[i]) == 0:  # sjekker om den deriverte er 0 så vi ikke får et udefinert uttrykk
             roots[i] = None
             steps[i] = None
-            print('Undefined')
             continue
 
         for j in range(0, max_steps):  # loop for å prøve å finne rota for x0
-            x1 = roots[i] - f(roots[i]) / df(roots[i])  # selve newton-raphson metoden
-            roots[i] = x1  # oppdaterer x-verdien for neste iterasjon
 
-            if j > max_steps:  # sjekker om vi har gått over max antall forsøk vi har bestemt
-                steps[i] = None
-                roots[i] = None
-                print('No roots found')
-                break
-            if not 10 * x0[0] < x1 < 10 * x0[-1]:  # luker ut gjetninger som går langt utenfor området
-                roots[i] = None
-                steps[i] = None
-                break
+            roots[i] += - f(roots[i]) / df(roots[i])  # selve newton-raphson metoden
 
-            if abs(f(x1)) < error_margin:  # sjekker om vi er nærme nok rota i forhold til feilmarginen vi har satt
-                steps[i] = j  # lagrer antall steg vi brukte for å finne rota
+            if j > max_steps or not 10 * x0[0] < roots[i] < 10 * x0[-1]:  # sjekker om vi har gått over max antall
+                steps[i] = None                                           # forsøk, og luker også ut gjetninger som går
+                roots[i] = None                                           # langt utenfor området, funker fint så lenge
+                break                                                     # intervallet er symmetrisk om y-aksen
+
+            if abs(f(roots[i])) < error_margin:  # sjekker om vi er nærme nok rota i forhold til marginen vi har satt
+                steps[i] = j                     # lagrer antall steg vi brukte for å finne rota
                 break
 
     return roots, steps
@@ -39,48 +33,60 @@ def newton_raphson(x0, f, df):
 
 def plot(x0, f, df):
     roots, steps = newton_raphson(x0, f, df)
+    root_values = print_roots(roots)
+
+    for i in range(len(root_values)):               # lager ikke et punkt for rota i grafen hvis den ikke er
+        if not x0[0] < root_values[i] < x0[-1]:     # innenfor intervallet vi ser på
+            del root_values[i]
+
     fig, axs = plt.subplots(2)
-    if flag == 1:
+
+    if flag == 1:   # overskrifter til grafene
         fig.suptitle('x from {} to {} with {} points'.format(x0[0], x0[-1], len(x0)))
     elif flag == 2:
         fig.suptitle(
             'x from {:.2f} to {:.2f} with {} points, and theta_1 = {:.2f}'.format(x0[0], x0[-1], len(x0), theta_1))
 
-    axs[0].plot(x0, f(x0), x0, df(x0))
+    axs[0].plot(x0, f(x0), x0, df(x0))   # øverste grafen med funksjonene f og df og punkter på røttene
     axs[0].legend(['f(x)', "f'(x)"])
     axs[0].grid(True)
     axs[0].set_xlabel("x")
     axs[0].set_ylabel("y")
+    axs[0].plot([i for i in root_values], np.zeros(len(root_values)), ls='', marker='o', c='r')  # punkter for røttene
 
-    axs[1].plot(x0, roots, x0, steps)
+    axs[1].plot(x0, roots, x0, steps)   # nederste grafen med en oversikt over røtter for hver x0 samt antall steg
     axs[1].legend(['roots', 'steps'])
     axs[1].grid(True)
     axs[1].set_xlabel("x0")
     axs[1].set_ylabel("x")
 
-    axs2 = axs[1].twinx()
-    axs2.plot(x0, roots, x0, steps)
-    axs2.set_ylabel("number of steps")
-    print_roots(roots)
+    axs_2 = axs[1].twinx()                   # kloner og speiler den nederste grafen slik at vi kan ha en label for
+    axs_2.set_ylabel("number of steps")      # hver side av y-aksen
+
     plt.show()
 
 
 def print_roots(roots):
     np.round_(roots, decimals=6, out=roots)                # runder av røttene til 6 desimaler så det er lettere å se
     values, counts = np.unique(roots, return_counts=True)  # hvilke som egentlig er like, og finner ut hvor mange ganger
-    ind = np.argpartition(-counts, kth=2)[:2]              # hver verdi dukker opp, printer ut de to røttene som
+    ind = np.argpartition(-counts, kth=2)[:2]              # hver verdi dukker opp, printer ut de to verdiene som
     print('\nRoots found:')                                # dukker opp flest ganger
+
     for i in ind:
         if flag == 1:
             print('x = {} evaluated from {} initial values'.format(values[i], counts[i]))
         elif flag == 2:
             print('x= {} evaluated from {} initial values, {:.2f} radians is {:.2f} degrees'.format(
-                values[i], counts[i], values[i], values[i]*(180/np.pi)))
+                values[i], counts[i], values[i], values[i]*(180/np.pi)))     # konverter også til grader for oppgave 2
+
     print('Out of a total of {} initial values evaluated, {} failed'.format(
         len(roots), len(roots) - counts[ind[0]] - counts[ind[1]]))
 
+    return [values[i] for i in ind]
+
 
 # oppgave 1
+
 def func_1(x):
     return 3 * x ** 2 + 4 * x - 4
 
@@ -90,12 +96,15 @@ def dfunc_1(x):
 
 
 flag = 1
+
 # regner ut med forskjellige intervaller av x0-verdier for å studere oppførselen til metoden
+
 plot(np.linspace(-1, 1, 1000), func_1, dfunc_1)
 plot(np.linspace(-5, 5, 1000), func_1, dfunc_1)
 
 
 # oppgave 2
+
 def func_2(x):
     return (34 * np.cos(theta_1 - x) - 40 * np.cos(theta_1) - 170 * np.cos(x) - 24 * np.sin(theta_1)
             - 102 * np.sin(x) + 163.25)
@@ -106,6 +115,7 @@ def dfunc_2(x):
 
 
 flag = 2
+
 # plotter de forskjellige theta_1 verdiene, med samme intervall fordi denne funksjonen er periodevis og vi er kun
 # interessert i de to røttene som er innenfor samme periode
 
